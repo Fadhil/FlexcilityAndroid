eec01d5a5701e25a25b5127755a43f70eec2d529
Fadhil Luqman <fadhil.luqman@gmail.com>
2016-10-20 12:27:10 +0800

    Dynamically load Dashboard Fragment

--- app/src/main/java/com/jomcode/flexcility/MainActivity.java
+++ app/src/main/java/com/jomcode/flexcility/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -104,17 +105,27 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        Fragment fragment = new Fragment();
+
 
         if (id == R.id.nav_dashboard) {
             Log.d("DEBUG", "In Dashboard Menu");
+            fragment = new DashboardFragment();
         } else if (id == R.id.nav_projects) {
-
+            Log.d("DEBUG", "In Projects Index");
+            //fragment = new ProjectFragment();
         } else if (id == R.id.nav_work_requests) {
 
         } else if (id == R.id.nav_work_orders) {
 
         }
 
+        fragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, fragment).commit();
+        item.setChecked(true);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
